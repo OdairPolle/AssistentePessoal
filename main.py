6513ffc08e3cd5190345dc452012c9652e780387
OdairPolle <odair.polle@hotmail.com>
2024-06-04 15:43:01 -0300

    add paginas

--- main.py
+++ main.py
@@ -2,9 +2,36 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/carteira')
+def carteira():
+    return None
+
+
+@app.route('/dispensa')
+def dispensa():
+    return None
+
+
+@app.route('/estudo')
+def estudo():
+    return None
+
+
+@app.route('/compromisso')
+def compromisso():
+    return None
+
+
+@app.route('/rotina')
+def rotina():
+    return None
 
 
 if __name__ =='__main__':
